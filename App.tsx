6b16aee35c0da1ab8633de76c17c743bb3e472ab
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2023-08-04 11:12:39 +0530

    first commit

---
+++ App.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { SafeAreaView, StyleSheet, Switch, Text } from "react-native";
+import { Colors } from "./src/styles/Colors";
+import { ThemeContext } from "./src/context/ThemeContext";
+import Keyboard from "./src/components/Keyboard";
+
+export default function App() {
+  const [theme, setTheme] = useState("light");
+  return (
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView
+        style={
+          theme === "light"
+            ? styles.container
+            : [styles.container, { backgroundColor: "black" }]
+        }
+      >
+        <Switch
+          value={theme === "dark"}
+          onValueChange={() => setTheme(theme === "light" ? "dark" : "light")}
+        />
+        <Keyboard />
+      </SafeAreaView>
+    </ThemeContext.Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.light,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+});
